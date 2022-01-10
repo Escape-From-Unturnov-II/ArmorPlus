f0af1e302f4c77dffa8534ed37d6fbcb35a05f5f
Michael Wachlhofer <38493853+DerSpeedMann@users.noreply.github.com>
2022-01-10 02:59:33 +0100

    added mask extension

---
+++ PvPRework/Models/Config/MaskExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedMann.PvPRework.Models.Config
+{
+    public class MaskExtension : ItemExtension
+    {
+        public bool ProtectOnlyFace = false; // if this mask only protects the face
+        public MaskExtension()
+        {
+
+        }
+    }
+}
